d591ffbb2c9f032b17f065c3fd957d748c299e72
iamchirag06 <singhchirag494@gmail.com>
2025-07-29 10:04:58 +0530

    New

--- OOPs 5A/Lab2_Q1.java
+++ OOPs 5A/Lab2_Q1.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Lab2 {
+public class Lab2_Q1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter an Integer:");
-        int input = sc.nextInt();
-
-        IntegerChecker checker = new IntegerChecker(input);
-        checker.displayResult();
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter an Integer:");
+            int input = sc.nextInt();
+            
+            IntegerChecker checker = new IntegerChecker(input);
+            checker.displayResult();
+        }
     }
 
     public static abstract class NumberChecker<T> {
@@ -35,6 +35,7 @@ public class Lab2 {
             super(number);
         }
 
+        @SuppressWarnings("override")
         public boolean isEven() {
             return number % 2 == 0;
         }
---
+++ OOPs 5A/Lab2_Q2.java
@@ -0,0 +1,37 @@
+// Java Program using Abstract Class (Example: Student Record) 
+// Abstract class 
+
+abstract class StudentRecord {
+
+    // Abstract method (no body) — specification abstraction
+    public abstract void showFees();
+
+    // Concrete method — implemented
+    public void showInfo() {
+        String name = "Raj";
+        int rollNo = 12;
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
+    }
+}
+
+// Subclass extends abstract class
+class CollegeStudent extends StudentRecord {
+    // Implementing abstract method
+    @SuppressWarnings("override")
+    public void showFees() {
+        int fees = 50000;
+        System.out.println("College Fees: Rs" + fees);
+    }
+}
+
+// Main class
+public class Lab2_Q2 {
+    public static void main(String[] args) {
+        // Creating object of subclass
+        CollegeStudent student = new CollegeStudent();
+        // Calling methods
+        student.showInfo(); // from abstract class
+        student.showFees(); // implemented in subclass
+    }
+}
