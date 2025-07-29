914a11c0f9d56dbec050e0af56b1fa3d0dd720e8
iamchirag06 <singhchirag494@gmail.com>
2025-07-29 10:01:19 +0530

    New

---
+++ OOPs 5A/Lab2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Lab2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an Integer:");
+        int input = sc.nextInt();
+
+        IntegerChecker checker = new IntegerChecker(input);
+        checker.displayResult();
+        sc.close();
+    }
+
+    public static abstract class NumberChecker<T> {
+        protected T number;
+
+        public NumberChecker(T number) {
+            this.number = number;
+        }
+
+        public abstract boolean isEven();
+
+        public void displayResult() {
+            System.out.println("Number entered: " + number);
+            if (isEven()) {
+                System.out.println(number + " is Even.");
+            } else {
+                System.out.println(number + " is Odd.");
+            }
+        }
+    }
+
+    public static class IntegerChecker extends NumberChecker<Integer> {
+        public IntegerChecker(Integer number) {
+            super(number);
+        }
+
+        public boolean isEven() {
+            return number % 2 == 0;
+        }
+    }
+}
